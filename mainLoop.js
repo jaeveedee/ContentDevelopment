65083dd9e5c4a7b198e27ae5317364ca8b79e08c
Devin Dominguez <dominguez.devin@gmail.com>
2014-05-29 14:13:06 -0700

    less auto

--- mainLoop.js
+++ mainLoop.js
@@ -17,7 +17,7 @@ function unlock() {
 
 }
 
-for(i = 0; i < 128; i++) {
+for(i = 0; i < 64; i++) {
 	emitters.push(new emitter(Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * 10 + 2));
 }
 
