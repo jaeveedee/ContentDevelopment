acfe62c984a7615f6506f1bbf538d2df9521a163
Jay Van Dyke <jaeveedee@me.com>
2014-06-04 23:44:55 -0700

    added button functions

---
+++ js/buttons.js
@@ -0,0 +1,21 @@
+var buttonOne = document.getElementById("buttonOne");
+var buttonTwo = document.getElementById("buttonTwo");
+var buttonThree = document.getElementById("buttonThree");
+var buttonFour = document.getElementById("buttonFour");
+
+buttonOne.addEventListener('click', function(){
+	console.log("click one");
+
+});
+buttonTwo.addEventListener('click', function(){
+	console.log("click two");
+
+});
+buttonThree.addEventListener('click', function(){
+	console.log("click three");
+
+});
+buttonFour.addEventListener('click', function(){
+	console.log("click four");
+
+});
